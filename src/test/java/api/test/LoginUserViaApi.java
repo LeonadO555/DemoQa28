c3cbae330c3572b76f36c0e22c95418d487c4675
Leonid Kazarin <l.kazarin@me.com>
2023-11-08 13:23:42 +0100

    Added LoginUserViaApi Test

--- src/test/java/api/test/LoginUserViaApi.java
+++ src/test/java/api/test/LoginUserViaApi.java
@@ -12,9 +12,25 @@ public class LoginUserViaApi {
     Faker faker = new Faker();
 
     @Test
-    public void LoginUserViaApiTest() {
+    public void successfulLoginUserViaApiTest() {
         String username = "Leonid";
         String password = "j4RMm9B*L";
+        authorizationAPI = new AuthorizationAPI();
+        Response response = authorizationAPI.login(username, password, 201);
+    }
+
+    @Test
+    public void invalidUsernameLoginUserViaApiTest() {
+        String username = "Leonid@@@___1111";
+        String password = "j4RMm9B*L";
+        authorizationAPI = new AuthorizationAPI();
+        Response response = authorizationAPI.login(username, password, 201);
+    }
+
+    @Test
+    public void invalidPasswordLoginUserViaApiTest() {
+        String username = "Leonid";
+        String password = "LLLLLLL";
         authorizationAPI = new AuthorizationAPI();
         Response response = authorizationAPI.login(username, password, 201);
     }
